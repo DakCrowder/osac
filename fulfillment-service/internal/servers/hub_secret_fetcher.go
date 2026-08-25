8fbc50962f73e022fa6c28bdf8a3c54cbe21b2e2
Dakota Crowder <dcrowder@redhat.com>
2026-08-25 11:15:07 -0400

    OSAC-2974: Fix imports

--- fulfillment-service/internal/servers/hub_secret_fetcher.go
+++ fulfillment-service/internal/servers/hub_secret_fetcher.go
@@ -18,9 +18,7 @@ import (
 	"errors"
 
 	"google.golang.org/grpc/codes"
-	grpccodes "google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	grpcstatus "google.golang.org/grpc/status"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	clnt "sigs.k8s.io/controller-runtime/pkg/client"
@@ -97,7 +95,7 @@ func (f *hubSecretFetcher) Fetch(ctx context.Context, coordinates map[string]str
 		Name:      secretName,
 	}, secret)
 	if err != nil {
-		if grpcstatus.Code(err) == grpccodes.Unauthenticated {
+		if status.Code(err) == codes.Unauthenticated {
 			f.hubClientProvider.EvictClient(hubID)
 		}
 		if errors.Is(err, context.Canceled) {
